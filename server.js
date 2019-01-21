95af99b36bbcb2edc17e8b6d738ee92f20f77ccc
aparna <aparna.dbit.in>
2019-01-21 12:06:43 +0530

    Intial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8080;
+
+// Body-parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/test",(req,res)=>{
+	return res.send("Welcome to CSI DBIT!!!");
+});
+
+// Server start
+app.listen(port, (req, res) => {
+    console.log("Listening to port");
+});
